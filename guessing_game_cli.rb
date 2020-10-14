8dc21c30a2c40248688c3094d2f1a2e8fc47afa1
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-10-14 22:58:15 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,9 @@
 # Code your solution here!
 
 def run_guessing_game
-  input = gets.
-  if input == "exit"
-    puts "Goodbye!"
-  end
+  num = rand(1..6)
+  puts "Guess a number from 1 to 6."
+  user_input = gets.chomp
+  if user_input == num
+    
 end
