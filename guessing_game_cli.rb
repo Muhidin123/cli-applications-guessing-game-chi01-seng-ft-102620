789e8376578da03aa67d1ff33f070e4398741ee8
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-10-14 22:58:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,5 +5,8 @@ def run_guessing_game
   puts "Guess a number from 1 to 6."
   user_input = gets.chomp
   if user_input == num
+    puts "You guessed the correct number!"
+  elsif user_input != num
+    puts
     
 end
