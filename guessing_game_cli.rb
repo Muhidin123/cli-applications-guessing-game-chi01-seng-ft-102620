f2972b3ee295c202515e7d96085e3cf86e47a576
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-10-14 23:04:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   user_input = gets.chomp
   if user_input == num.to_s
     puts "You guessed the correct number!"
-  elsif user_input == "exit"
+  elsif user_input.downcase == "exit"
     puts "Goodbye"
   else user_input != num
     puts "Sorry! The computer guessed #{num}."
